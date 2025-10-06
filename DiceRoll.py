f27d7e723a5c007acc82c9f265d719aedba38f5d
jadonwiebe <jadonwiebe@gmail.com>
2025-10-06 04:54:28 +0000

    No idea what to do

--- DiceRoll.py
+++ DiceRoll.py
@@ -1,18 +1,26 @@
 #DiceRoll.py
-#Name:
-#Date:
-#Assignment:
+#Name:Jadon Wiebe
+#Date:10/05
+#Assignment:Dice Rolls
 import random
 
 def main():
   #Create an empty list with possible roll values
   rolls = [0,0,0,0,0,0,0,0,0,0,0,0]
   #Create two dice values ranging from 1 - 6 each
-  
+  for r in range(10000):
+    dice1 = random.randint(1,6)
+    rolls[dice1 - 1] = rolls[dice1 - 1] + 1
+    dice2 = random.randint(1, 6)
+    rolls[dice2 - 1] = rolls[dice2 - 1] +1
+    
   #find the sum total of the two dice
-  
-  #print statictics for dice rolls
 
+  #print statictics for dice rolls
+  dice = 2
+  for count in rolls:
+    print(dice, ":", count)
+    dice = dice + 1
 
 if __name__ == '__main__':
   main()
